b273540e8ccc19e0f15b098b01cdd76bcd65c754
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-06-12 19:43:28 +0200

    Added jei-devkit-angular-shared library

--- src/app/rxjs-layout-page/rxjs-layout-page.module.ts
+++ src/app/rxjs-layout-page/rxjs-layout-page.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { HeaderPrintModule } from '../components/header-print/header-print.module';
-import { ResponsiveLayoutModule } from '../components/responsive-layout/responsive-layout.module';
+import { HeaderPrintComponent, ResponsiveLayoutComponent } from '@jaimemartinmartin15/jei-devkit-angular-shared';
 import { RxjsLayoutPageRoutingModule } from './rxjs-layout-page-routing.module';
 import { RxjsLayoutPageComponent } from './rxjs-layout-page.component';
 import { RxjsLayoutPageResolver } from './rxjs-layout-page.resolver';
 
 @NgModule({
-  imports: [CommonModule, RxjsLayoutPageRoutingModule, HeaderPrintModule, ResponsiveLayoutModule],
+  imports: [CommonModule, RxjsLayoutPageRoutingModule, HeaderPrintComponent, ResponsiveLayoutComponent],
   declarations: [RxjsLayoutPageComponent],
   providers: [RxjsLayoutPageResolver],
 })
